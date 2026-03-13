098714fbd512822f57b0725c6f269f7399dab15a
Berkay Kömür <berkaykmr0@gmail.com>
2026-03-13 12:09:22 +0300

    refactor: improve CustomerAddress entity, service logic and controller

---
+++ src/main/java/com/berkaykomur/ordermanagement/security/CustomUserDetails.java
@@ -0,0 +1,45 @@
+package com.berkaykomur.ordermanagement.security;
+
+import com.berkaykomur.ordermanagement.entity.User;
+import com.berkaykomur.ordermanagement.enums.Role;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+public class CustomUserDetails implements UserDetails {
+
+    private Long id;
+    private String username;
+    private String password;
+    private Role role;
+
+    public CustomUserDetails(User user ) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.role = user.getRole();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
+}
